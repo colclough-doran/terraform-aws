9c55dd8b166ab6665d3fd47e33ee51746e02da56
Colclough Doran <devops.codo@pm.me>
2026-06-10 15:15:59 +0100

    Created project scaffolding

--- infrastructure/vpc/main.tf
+++ infrastructure/vpc/main.tf
@@ -3,9 +3,9 @@ module "dataops_vpc" {
 
   aws_region        = var.aws_region
   environment       = var.environment
-  project_scope      = var.project_scope
+  project_scope     = var.project_scope
   subnet_cidr_block = var.subnet_cidr_block
-  
-  vpc_cidr_block    = var.vpc_cidr_block
-  vpc_subnet_azs    = var.vpc_subnet_azs
+
+  vpc_cidr_block = var.vpc_cidr_block
+  vpc_subnet_azs = var.vpc_subnet_azs
 }
