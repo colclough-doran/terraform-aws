462704fd52f57d398b59ecb537582b4af59a1013
Colclough Doran <devops.codo@pm.me>
2026-06-11 20:17:45 +0100

    Updated jenkins-sg module, .gitignore and added data.tf

--- infrastructure/ec2/security-groups/data.tf
+++ infrastructure/ec2/security-groups/data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket  = "dataops-sandbox-infrastructure"
+    bucket  = "cd-tfstate-sandbox"
     key     = "vpc/terraform.tfstate"
     region  = "eu-west-1"
     profile = "sandbox"
